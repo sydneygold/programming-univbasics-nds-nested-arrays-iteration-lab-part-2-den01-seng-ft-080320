2d91c7d5f136fa67604139e9bf5effdc130ae493
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-19 19:55:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ outer_results = []
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    min_in_nested_arrays =
+    min_in_nested_arrays = src[row_index][element_index].compact.min
     while element_index < src[row_index].count do
       if src[row_index][element_index] < min_in_nested_arrays.length
         p src[row_index][element_index]
